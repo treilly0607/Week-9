d8480b829fb7b7ac51fb0aebdf7b3a5fdc589b6c
treilly0607 <60261903+treilly0607@users.noreply.github.com>
2020-11-23 11:54:07 +0000

    Added song objects

--- Programming Practice/Program.cs
+++ Programming Practice/Program.cs
@@ -13,7 +13,12 @@ namespace Programming_Practice
     {
         static void Main(string[] args)
         {
-            
+            // Song Objects
+            Song s1 = new Song("BEUTIFUL PEOPLE","ED SHEERIN", 3.15, Genre.Pop);
+            Song s2 = new Song("DANCE MONKEY", "TONES AND I", 4.20, Genre.Dance);
+            Song s3 = new Song("CIRCLES", "POST MALONE", 3.25, Genre.Pop);
+            Song s4 = new Song("RIDE IT", "REGARD", 3.37, Genre.Dance);
+            Song s5 = new Song("SOUTH OF THE BORDER", "ED SHEERIN", 4.26, Genre.Pop);
         }
     }
 }
