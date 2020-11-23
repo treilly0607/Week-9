b3230013360add491f689a359b653dafd72451c8
treilly0607 <60261903+treilly0607@users.noreply.github.com>
2020-11-23 11:30:58 +0000

    Added Properties

--- Programming Practice/Song.cs
+++ Programming Practice/Song.cs
@@ -4,14 +4,23 @@ using System.Text;
 
 namespace Programming_Practice
 {
-    class Song
+    public class Song
     {
-        enum Genre
-        {
-            Rock,
-            Pop,
-            Dance,
-            Other
-        }
+
+        //properties
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public double Duration { get; set; }
+        public Genre MusicGenre { get; set; } 
+
+
+
+    }
+    public enum Genre // enum for genre
+    {
+        Rock,
+        Pop,
+        Dance,
+        Other
     }
 }
