e0604020a4ea6dd8d92d3c18e4c130d129a1d770
treilly0607 <60261903+treilly0607@users.noreply.github.com>
2020-11-23 10:52:52 +0000

    Add project files.

---
+++ Programming Practice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Programming_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
