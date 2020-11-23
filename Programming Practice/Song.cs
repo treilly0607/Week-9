d7d4df78ddb92d2f0b21a9497535c7ba8b99c50d
treilly0607 <60261903+treilly0607@users.noreply.github.com>
2020-11-23 11:45:40 +0000

    Added Contrusctors

--- Programming Practice/Song.cs
+++ Programming Practice/Song.cs
@@ -7,14 +7,25 @@ namespace Programming_Practice
     public class Song
     {
 
-        //properties
+        //  Properties
         public string Title { get; set; }
         public string Artist { get; set; }
         public double Duration { get; set; }
         public Genre MusicGenre { get; set; } 
 
-
-
+        //  Contructors
+        public Song(string title, string artist, double duration, Genre musicGenre)
+        {
+            Title = title;
+            Artist = artist;
+            Duration = duration;
+            MusicGenre = musicGenre;
+        }
+        // Default Contructors if duration and genre unknown
+        public Song(string title, string artist) : this(title,artist,0,Genre.Other) { }
+        // Default Contructors if all unknown
+        public Song() : this("Unknown", "Unknown") { }
+       
     }
     public enum Genre // enum for genre
     {
